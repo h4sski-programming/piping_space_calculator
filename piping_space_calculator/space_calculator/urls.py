199c24ec8359bb850de4a0b9285b8cef86e4e36b
h4sski.programming <h4sski.programming@gmail.com>
2023-12-05 23:04:24 +0100

    add bbb view, calculating space value,  get rows for all pipe dims

--- piping_space_calculator/space_calculator/urls.py
+++ piping_space_calculator/space_calculator/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("aaa/", views.aaa, name="aaa"),
+    path("bbb/", views.bbb, name="bbb"),
     path("hello/", views.hello, name="hello"),
 ]
