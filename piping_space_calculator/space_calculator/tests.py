128615a3758f62156f5e1f31efa6d5eb20eee66a
h4sski.programming <h4sski.programming@gmail.com>
2023-12-06 16:28:38 +0100

    tests are working. I had to save the file, only that

--- piping_space_calculator/space_calculator/tests.py
+++ piping_space_calculator/space_calculator/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .views import index, aaa, bbb
 
 # Create your tests here.
+
+class ViewsTests(TestCase):
+    
+    
+    def test_index_view(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+    
+    
+    def test_aaa_view(self):
+        response = self.client.get(reverse('aaa'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'aaa.html')
+    
+    
+    def test_bbb_view(self):
+        response = self.client.get(reverse('bbb'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bbb.html')
+    
